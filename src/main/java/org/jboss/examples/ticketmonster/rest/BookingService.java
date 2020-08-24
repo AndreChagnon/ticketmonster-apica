f9003657a0f9a078a155b8c7f3efda5fce43ca7b
AndreChagnon <46169930+AndreChagnon@users.noreply.github.com>
2020-08-24 17:15:58 -0400

    Update BookingService.java
    adjusted sleep time to 8000

--- src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
+++ src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
@@ -125,7 +125,7 @@ public class BookingService extends BaseEntityService<Booking> {
     public Response createBooking(BookingRequest bookingRequest) {
         try {
             
-            //Thread.sleep6000);
+            //Thread.sleep8000);
 
             // identify the ticket price categories in this request
             Set<Long> priceCategoryIds = bookingRequest.getUniquePriceCategoryIds();
